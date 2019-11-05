c52b7fb8a028dd784e1c8d6db60f5ad5bea7bf83
Kyle Barnett <kbarnett23@gmail.com>
2019-11-05 15:09:02 -0700

    logger function working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const server = express();
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+server.use(logger);
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +14,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  let date = new Date();
+  console.log(`TIMESTAMP: ${date} URL: ${req.url} METHOD: ${req.method} request!`);
+  next();
 };
 
 
